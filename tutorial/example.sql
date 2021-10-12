1e8a57b2b6249171cbf54ec5c0a3a17c38626510
c-jamie <jamie.b.clery@gmail.com>
2021-10-12 19:57:55 +0100

    fix: example

--- tutorial/example.sql
+++ tutorial/example.sql
@@ -9,9 +9,11 @@
   [sqlmend]
 */
 
-{{if )}}
+{{if abc_2)}}
+-- if the second migration is active, we have access to this column
 select name, name_type from {{.table1}}
 {{else}}
 select name null as name_type from {{.table1}}
+{{end}}
 
 
